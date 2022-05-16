273100d80e343ca840a5699adbd37cd865439634
Raquel Teixeira <raquelat@ciandt.com>
2022-05-16 14:24:35 -0300

    cinco

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -4,7 +4,7 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        String valorFormatado = "Valor: R" + NumberFormat.getCurrencyInstance().format(valor);
+        String valorFormatado = "Valor: " + NumberFormat.getCurrencyInstance().format(valor);
         System.out.printf("%s%n%s%.2f%%%n",valorFormatado, "Taxa: ", taxa);
     }
 }
