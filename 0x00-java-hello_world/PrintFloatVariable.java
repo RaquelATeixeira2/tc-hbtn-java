a243791a677b45f84715c16eb4de05be415b750c
Raquel Teixeira <raquelat@ciandt.com>
2022-05-16 12:20:16 -0300

    flot

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,8 +1,6 @@
-import java.nio.channels.Pipe;
-
 public class PrintFloatVariable {
     public static void main(String[] args){
         float pi = 3.14159f;
-        System.out.printf("%.3f", pi);
+        System.out.printf("%s%.3f","Valor de pi:", pi);
     }
 }
