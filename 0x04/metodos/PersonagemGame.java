6ebb9a39abad7faf74bbff2a1d5a9f96e7483676
Raquel Teixeira <raquelat@ciandt.com>
2022-05-17 17:30:53 -0300

    objetos e classes

--- 0x04/metodos/PersonagemGame.java
+++ 0x04/metodos/PersonagemGame.java
@@ -1,34 +1,39 @@
 public class PersonagemGame {
     private int saudeAtual;
-    private String nome;    
+    private String nome;
 
-    public void tomarDano(int quantidadeDeDano){
-        if(saudeAtual >= 0){
+    public void tomarDano(int quantidadeDeDano) {
+        if (saudeAtual > 0) {
             saudeAtual = saudeAtual - quantidadeDeDano;
+            if (saudeAtual < 0) {
+                saudeAtual = 0;
+            }
         }
     }
 
-    public void receberCura(int quantidadeDeCura){
-        if(saudeAtual <= 100){
+    public void receberCura(int quantidadeDeCura) {
+        if (saudeAtual <= 100) {
             saudeAtual = saudeAtual + quantidadeDeCura;
+            if (saudeAtual > 100) {
+                saudeAtual = 100;
+            }
         }
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public int getSaudeAtual(){
+    public int getSaudeAtual() {
         return saudeAtual;
     }
 
-    public void setSaudeAtual(int saudeAtual){
+    public void setSaudeAtual(int saudeAtual) {
         this.saudeAtual = saudeAtual;
     }
-
 
 }
