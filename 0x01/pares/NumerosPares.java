bfd8168509ab4d16a7164640f6423b435e3233b6
Raquel Teixeira <raquelat@ciandt.com>
2022-05-23 11:03:45 -0300

    numerosPares

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,13 @@
+public class NumerosPares {
+    public static void main(String[] args){
+        int i;
+        for(i = 0; i < 100; i++){
+            if(i > 98){
+                System.out.print(i+"\n");
+            }
+            else{
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
