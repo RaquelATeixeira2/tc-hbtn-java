5dd09357dca3c5505e8f617fcb6ec1ca29240940
Raquel Teixeira <raquelat@ciandt.com>
2022-05-23 14:14:22 -0300

    quadrado

---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,25 @@
+public class Quadrado extends Retangulo{
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado, double altura, double largura) {
+        if(lado < 0){
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+        this.lado = lado;
+        this.largura = largura;
+        this.altura = altura;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        String r = String.format("[Quadrado] %.2f", lado);
+        return r;
+    }
+    
+}
