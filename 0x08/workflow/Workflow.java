5135a1b603b257e411e1dcac1b89d5ace46e92a4
Raquel Teixeira <raquelat@ciandt.com>
2022-05-25 13:08:00 -0300

    fixacao interface

---
+++ 0x08/workflow/Workflow.java
@@ -0,0 +1,14 @@
+import atividade.Atividade;
+import java.util.ArrayList;
+
+public class Workflow {
+    public ArrayList<Atividade> atividades = new ArrayList<>();
+
+    public ArrayList<Atividade> getAtividades() {
+        return atividades;
+    }
+
+   public void registrarAtividade(Atividade atividade){
+       this.atividades.add(atividade);
+   }
+}
