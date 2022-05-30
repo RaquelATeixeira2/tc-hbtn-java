8ece0b7b2391d9ae5c56be71800c869a8196525f
Raquel Teixeira <raquelat@ciandt.com>
2022-05-30 14:29:44 -0300

    list

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ListaTodo {
+    private ArrayList<Tarefa> tarefas;
+
+    public ListaTodo() {
+        this.tarefas = tarefas;
+    }
+
+    public void adicionarTarefa(Tarefa t) throws Exception{
+        for(Tarefa tarefa : tarefas){
+            if(t.getIdentificador() == tarefa.getIdentificador()){
+                throw new Exception("Tarefa com identificador " + t.getIdentificador() + " ja existente na lista");
+            }
+        }
+        tarefas.add(t);
+    }
+
+    public boolean marcarTarefaFeita(int identificador){
+        for(Tarefa tarefa : tarefas){
+            if(tarefa.getIdentificador() == identificador){
+                tarefa.setEstahFeita(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean desfazerTarefa(int identificador){
+        for(Tarefa tarefa : tarefas){
+            if(tarefa.getIdentificador() == identificador){
+                tarefa.setEstahFeita(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void desfazerTodas(){
+        for(Tarefa tarefa : tarefas){
+            tarefa.setEstahFeita(false);
+        }
+    }
+
+    public void fazerTodas(){
+        for(Tarefa tarefa : tarefas){
+            tarefa.setEstahFeita(true);
+        }
+    }
+
+    public void listarTarefas(){
+        for(Tarefa tarefa : tarefas){
+           if(tarefa.isEstahFeita()){
+                System.out.println("[X] id: " + tarefa.getIdentificador() + " - Descrição: " + tarefa.getDescricao());
+           } else{
+            System.out.println("[ ] id: " + tarefa.getIdentificador() + " - Descrição: " + tarefa.getDescricao());
+           }
+        }
+    }
+
+}
