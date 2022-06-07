440a641d66a4588bec787c23e083bb1e0f3d1194
Raquel Teixeira <raquelat@ciandt.com>
2022-06-06 21:58:14 -0300

    excecoes

--- 0x03/conta_basica/ContaBancariaBasica.java
+++ 0x03/conta_basica/ContaBancariaBasica.java
@@ -23,44 +23,38 @@ public class ContaBancariaBasica {
     }
 
     public void depositar(double valor) throws OperacaoInvalidaException{
-        if(valor < 0){
+        if (valor <= 0) {
             throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
         }
-
-        this.saldo = valor;
+        this.saldo += valor;
     }
 
     public void sacar(double valor) throws OperacaoInvalidaException{
         if(valor < 0){
-            throw new OperacaoInvalidaException("Valor de saque deve ser menor que 0");
-        } 
-        if(valor > saldo){
+            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+        } else if(valor > this.saldo){
             throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
         }
         this.saldo = saldo - valor;
     }
 
     public double calcularTarifaMensal(){
-        double tarifa10 = 10.0;
-        double tarifa10p = saldo * 0.1;
-        double tarifa = 0;
-        if(tarifa10 > tarifa10p){
-            tarifa = tarifa10p;
-        } else {
-            tarifa = tarifa10;
+        double tarifa = this.saldo * 0.1;
+        if(tarifa > 10){
+            return 10;
         }
         return tarifa;
     }
 
     public double calcularJurosMensal(){
-        double jurosM = taxaJurosAnual / 12;
-        if(saldo < 0){
-            jurosM = 0;
+        double jurosM = 0;
+        if(saldo > 0){
+            jurosM = getSaldo() * (taxaJurosAnual/100) / 12;
         }
         return jurosM;
     }
 
     public void aplicarAtualizacaoMensal(){
-        this.saldo = (saldo - calcularTarifaMensal()) + calcularJurosMensal(); 
+        this.saldo = getSaldo() - calcularTarifaMensal() + calcularJurosMensal(); 
     }
 }
