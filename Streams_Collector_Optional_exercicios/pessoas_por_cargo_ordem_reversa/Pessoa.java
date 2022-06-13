c27d72909cb295c9ffaa3c78266a97c96ed23cc9
Raquel Teixeira <raquelat@ciandt.com>
2022-06-13 18:33:51 -0300

    options

--- Streams_Collector_Optional_exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
+++ Streams_Collector_Optional_exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -1,10 +1,17 @@
-public class Pessoa {
+public class Pessoa implements Comparable<Pessoa>{
     private int codigo;
     private String nome;
     private String cargo;
     private int idade;
     private int salario;
-    
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, int salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
     public int getCodigo() {
         return codigo;
     }
@@ -23,5 +30,48 @@ public class Pessoa {
     @Override
     public String toString() {
         return "[" + this.codigo + "] " + " " + this.nome + " " + this.cargo + " R$" + this.salario;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cargo == null) ? 0 : cargo.hashCode());
+        result = prime * result + codigo;
+        result = prime * result + idade;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + salario;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (cargo == null) {
+            if (other.cargo != null)
+                return false;
+        } else if (!cargo.equals(other.cargo))
+            return false;
+        if (codigo != other.codigo)
+            return false;
+        if (idade != other.idade)
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (salario != other.salario)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return this.nome.compareTo(pessoa.getNome());
     }
 }
